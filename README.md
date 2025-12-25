d9fb82e3b20d6dd9200bcab059b55baec0066979
vatsalkansagara <vatsalkansagara246@gmail.com>
2025-12-25 16:00:01 +0530

    new change

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Myprotfolio
+# Myprotfolio VatsalKansagara
--- js/script.js
+++ js/script.js
@@ -53,7 +53,7 @@ function(){
 
 // <!-- typed js effect starts -->
     var typed = new Typed(".typing-text", {
-        strings: ["Front-End Developer"],
+        strings: ["Web Developer"],
         loop: true,
         typeSpeed: 50,
 		backSpeed: 25,
